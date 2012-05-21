079cd7654355d42b6c4a66474aa7eccc4992b95a
Roy Tu <thatsnail@gmail.com>
2012-05-21 10:04:44 -0400

    String building function

--- src/MusicGenerator.java
+++ src/MusicGenerator.java
@@ -1,18 +1,24 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 import org.jfugue.*;
 
 public class MusicGenerator {
 	MusicGenerator(){
-		char[] Notes={'C','D','E','F','G','A','B'};
+		char[] Tones={'C','D','E','F','G','A','B'};
 		Random random = new Random();
-		System.out.println(Notes[random.nextInt(7)]);
 		
-		String result="";
-		for(int i=0;i<10;i++){
-			result+=Notes[random.nextInt(7)]+" ";
-		}
+		ArrayList<Note> notes=new ArrayList();
+		
+		Note note=new Note();
+		note.tone=Tones[random.nextInt(7)];
+		note.octave=random.nextInt(3)+3;
+		notes.add(note);
+		
+		String result=buildString(notes);
+		
+		System.out.println(result);
 		
 		Player player = new Player();
 		Pattern pattern = new Pattern(result);
@@ -21,5 +27,18 @@ public class MusicGenerator {
 	
 	public static void main(String[] args){
 		new MusicGenerator();
+	}
+	
+	public static String buildString(ArrayList<Note> notes){
+		//TODO: Build string based on NoteArray
+		String fin="";
+		for(int i=0;i<notes.size();i++){
+			Note cnote=notes.get(i);
+			fin+=cnote.tone;
+			fin+=cnote.octave;
+			fin+=" ";
+		}
+		
+		return fin;
 	}
 }
